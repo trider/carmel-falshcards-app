6cae12fdd6e13c50b0f8d66ac416e8619d8457c7
Jonathan Gold <jonathangold@Jonathans-MacBook-Air.local>
2018-05-02 17:12:43 +0300

    Home-4 Ver 0.1: Branch commit

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react';
-import logo from '../img/logo.svg';
-import '../styles/App.css';
 import Welcome from './Welcome';
-import * as data from '../data/data'
+import * as data from '../data/data';
+import '../styles/App.css';
 
 class Terms extends Component {
 	
+	getTerms() {
+
+		const terms = data.terms.map((term) =>
+					<div className="Term-container" key={term.id}>
+						<p className="Term-title">{term.name}</p>
+						<p className="Term-body">{term.description}</p>
+					</div>
+  );
+  return terms
+	}
 
 
 	render() {
-		const terms = JSON.stringify(data.terms)
-		const list = data.terms
+		
 		return (
 			<div>
 				<Welcome name={'Terms for ' + this.props.name} />
-				<p>{terms}</p>
+				{this.getTerms()}
 			</div>
 		);
 	}
